d8941acf7755d5b8777c4b9b8fdaf148f8318ecb
nutjeep <anajibmuhammad@gmail.com>
2022-01-19 17:01:03 +0700

    update navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,8 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
+
+function toggle() {
+    var blur = document.querySelector('.destination', '.branding');
+    blur.classList.toggle('blur-effect')
+}
