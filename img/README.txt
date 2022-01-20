74f5622848bff9f22f1ae449c2144f0181ebea7c
nutjeep <anajibmuhammad@gmail.com>
2022-01-20 12:13:06 +0700

    adding carousel

---
+++ img/README.txt
@@ -0,0 +1,3 @@
+Image size for slider : 3500 x 2300 px
+Image size for Destination :
+Image size for gallery : 3500 x 2500 px
--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ menuToggle.addEventListener('click', function() {
     nav.classList.toggle('slide');
 });
 
+// Blur Effect for Navbar - Mobile
 function toggle() {
     var blur = document.querySelector('.destination', '.branding');
     blur.classList.toggle('blur-effect')
 }
+
+
+// Stiky Navigation Bar
+window.addEventListener("scroll", function(){
+    var nav = document.querySelector("nav");
+    nav.classList.toggle("active", window.scrollY > 50);
+})
